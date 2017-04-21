c08649dadecce5c2d92303f72eede30ade05eaf2
Riku Rouvila <riku.rouvila@gmail.com>
2017-04-21 14:31:50 +0300

    style changes

--- src/App.js
+++ src/App.js
@@ -60,16 +60,16 @@ const AppContainer = styled.div`
 
 const Logo = styled.img`
   margin-right: -15px;
-  margin-top: 5px;
+  margin-top: -8px;
   width: 110px;
   @media (max-width: 380px) {
     flex-shrink: 1;
-    width: 100%;
+    width: 100px;
   }
 `;
 
 const Description = styled.div`
-  line-height: 1.5em;
+  line-height: 1.1em;
   color: #a0a0a0;
   margin-top: -19px;
   font-size: 19px;
@@ -135,12 +135,13 @@ const Hero = styled.div`
 
 const Title = styled.h1`
   font-size: 56px;
-  font-family: 'blenny', sans-serif;
+  font-weight: normal;
+  font-family: 'Fontdiner Swanky', cursive;
   margin-top: 0;
   margin-bottom: 0;
   color: #9143c1;
   @media (max-width: 380px) {
-    font-size: 40px;
+    font-size: 50px;
   }
 `;
 
@@ -165,9 +166,14 @@ const ShuffleButton = styled.button`
   border-radius: 5px;
   text-align: center;
   z-index: 2;
+  transition: transform 300ms;
+  box-shadow: 10px 10px 0px rgba(0, 0, 0, 0.1);
   @media (max-width: 600px) {
-    left: 1em;
-    width: auto;
+    width: 80%;
+    left: 10%;
+  }
+  &:hover {
+    transform: scale(1.1, 1.1);
   }
 `;
 
@@ -203,6 +209,7 @@ const Lot = styled.div`
   position: relative;
   cursor: pointer;
   min-width: 4em;
+  border-radius: 3px;
 `;
 
 const RemoveLot = styled.div`
@@ -241,6 +248,7 @@ const WinnerPopup = styled.div`
   z-index: 3;
   border: 5px solid #ffb100;
   background-color: #ffffff;
+  border-radius: 5px;
   background-image: url("data:image/svg+xml,%3Csvg width='180' height='180' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M81.28 88H68.413l19.298 19.298L81.28 88zm2.107 0h13.226L90 107.838 83.387 88zm15.334 0h12.866l-19.298 19.298L98.72 88zm-32.927-2.207L73.586 78h32.827l.5.5 7.294 7.293L115.414 87l-24.707 24.707-.707.707L64.586 87l1.207-1.207zm2.62.207L74 80.414 79.586 86H68.414zm16 0L90 80.414 95.586 86H84.414zm16 0L106 80.414 111.586 86h-11.172zm-8-6h11.173L98 85.586 92.414 80zM82 85.586L87.586 80H76.414L82 85.586zM17.414 0L.707 16.707 0 17.414V0h17.414zM4.28 0L0 12.838V0h4.28zm10.306 0L2.288 12.298 6.388 0h8.198zM180 17.414L162.586 0H180v17.414zM165.414 0l12.298 12.298L173.612 0h-8.198zM180 12.838L175.72 0H180v12.838zM0 163h16.413l.5.5 7.294 7.293L25.414 172l-8 8H0v-17zm0 10h6.613l-2.334 7H0v-7zm14.586 7l7-7H8.72l-2.333 7h8.2zM0 165.414L5.586 171H0v-5.586zM10.414 171L16 165.414 21.586 171H10.414zm-8-6h11.172L8 170.586 2.414 165zM180 163h-16.413l-7.794 7.793-1.207 1.207 8 8H180v-17zm-14.586 17l-7-7h12.865l2.333 7h-8.2zM180 173h-6.613l2.334 7H180v-7zm-21.586-2l5.586-5.586 5.586 5.586h-11.172zM180 165.414L174.414 171H180v-5.586zm-8 5.172l5.586-5.586h-11.172l5.586 5.586zM152.933 25.653l1.414 1.414-33.94 33.942-1.416-1.416 33.943-33.94zm1.414 127.28l-1.414 1.414-33.942-33.94 1.416-1.416 33.94 33.943zm-127.28 1.414l-1.414-1.414 33.94-33.942 1.416 1.416-33.943 33.94zm-1.414-127.28l1.414-1.414 33.942 33.94-1.416 1.416-33.94-33.943zM0 85c2.21 0 4 1.79 4 4s-1.79 4-4 4v-8zm180 0c-2.21 0-4 1.79-4 4s1.79 4 4 4v-8zM94 0c0 2.21-1.79 4-4 4s-4-1.79-4-4h8zm0 180c0-2.21-1.79-4-4-4s-4 1.79-4 4h8z' fill='%23eca417' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
   box-shadow: 20px 20px 0px rgba(0, 0, 0, 0.5);
   box-sizing: border-box;
@@ -307,6 +315,7 @@ const TwitterShareButtonContainer = styled(TwitterShareButton)`
 const Social = styled.div`
   display: flex;
   margin-top: 1em;
+  justify-content: center;
 `;
 
 const UrlShareButtonContainer = styled.div`
