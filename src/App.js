f35541306a6b4bab0ac78717d8b6e0cdd3f9c4ba
Riku Rouvila <riku.rouvila@gmail.com>
2017-04-22 01:08:29 +0300

    add eslint and hot reloading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import queryString from 'query-string';
 import styled from 'styled-components';
 import Textarea from 'react-textarea-autosize';
@@ -9,17 +9,17 @@ import loader from './loader.svg';
 import link from './link.svg';
 import close from './close.svg';
 
-import {ShareButtons, generateShareIcon} from 'react-share';
-import {shorten} from './service';
+import { ShareButtons, generateShareIcon } from 'react-share';
+import { shorten } from './service';
 import Guide from './components/Guide';
-
-const {FacebookShareButton, TwitterShareButton} = ShareButtons;
 
 import './App.css';
 
+const { FacebookShareButton, TwitterShareButton } = ShareButtons;
+
 const MIN_SHUFFLES = 30;
 
-const easeOutBy = power => t => 1 - Math.abs(Math.pow(t - 1, power));
+const easeOutBy = (power) => (t) => 1 - Math.abs(Math.pow(t - 1, power));
 
 const PLACEHOLDERS = [
   'pizza, kalakeitto, maksalaatikko...',
@@ -34,10 +34,10 @@ function createSeed(timeSeed) {
 function writeToUrl(state) {
   const storedState = {
     s: state.seed,
-    p: state.participants.map(({name}) => name).join(','),
+    p: state.participants.map(({ name }) => name).join(','),
   };
 
-  window.history.pushState('page2', 'Title', '/?' + queryString.stringify(storedState));
+  window.history.pushState('page2', 'Title', `/?${queryString.stringify(storedState)}`);
 }
 
 function getStoredState() {
@@ -45,7 +45,7 @@ function getStoredState() {
 
   return {
     seed: urlParams.s === undefined ? 0 : parseInt(urlParams.s, 10),
-    participants: urlParams.p ? urlParams.p.split(',').map((name, id) => ({name, id})) : [],
+    participants: urlParams.p ? urlParams.p.split(',').map((name, id) => ({ name, id })) : [],
   };
 }
 
@@ -59,7 +59,7 @@ function shuffleEasing(t) {
 }
 
 const AppContainer = styled.div`
-  padding-bottom: ${({padded}) => padded ? 220 : 0}px;
+  padding-bottom: ${({ padded }) => padded ? 220 : 0}px;
   transition: padding 300ms;
 `;
 
@@ -202,9 +202,9 @@ const Lot = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  background: ${({highlighted}) => highlighted ? '#ffb100' : '#65d065'};
-  z-index: ${({highlighted, winner}) => highlighted || winner ? 1 : 0};
-  ${({highlighted}) => highlighted ? 'transform: scale(1.5, 1.5)' : ''};
+  background: ${({ highlighted }) => highlighted ? '#ffb100' : '#65d065'};
+  z-index: ${({ highlighted, winner }) => highlighted || winner ? 1 : 0};
+  ${({ highlighted }) => highlighted ? 'transform: scale(1.5, 1.5)' : ''};
   transition: transform 200ms;
   margin: 1em 0.5em;
   padding: 0.5em;
@@ -255,8 +255,8 @@ const WinnerPopup = styled.div`
   padding: 0.5em;
   z-index: 3;
   border: 5px solid #ffb100;
-  background-color: #ffffff;
   border-radius: 5px;
+  background-color: #ffffff;
   background-image: url("data:image/svg+xml,%3Csvg width='180' height='180' viewBox='0 0 180 180' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M81.28 88H68.413l19.298 19.298L81.28 88zm2.107 0h13.226L90 107.838 83.387 88zm15.334 0h12.866l-19.298 19.298L98.72 88zm-32.927-2.207L73.586 78h32.827l.5.5 7.294 7.293L115.414 87l-24.707 24.707-.707.707L64.586 87l1.207-1.207zm2.62.207L74 80.414 79.586 86H68.414zm16 0L90 80.414 95.586 86H84.414zm16 0L106 80.414 111.586 86h-11.172zm-8-6h11.173L98 85.586 92.414 80zM82 85.586L87.586 80H76.414L82 85.586zM17.414 0L.707 16.707 0 17.414V0h17.414zM4.28 0L0 12.838V0h4.28zm10.306 0L2.288 12.298 6.388 0h8.198zM180 17.414L162.586 0H180v17.414zM165.414 0l12.298 12.298L173.612 0h-8.198zM180 12.838L175.72 0H180v12.838zM0 163h16.413l.5.5 7.294 7.293L25.414 172l-8 8H0v-17zm0 10h6.613l-2.334 7H0v-7zm14.586 7l7-7H8.72l-2.333 7h8.2zM0 165.414L5.586 171H0v-5.586zM10.414 171L16 165.414 21.586 171H10.414zm-8-6h11.172L8 170.586 2.414 165zM180 163h-16.413l-7.794 7.793-1.207 1.207 8 8H180v-17zm-14.586 17l-7-7h12.865l2.333 7h-8.2zM180 173h-6.613l2.334 7H180v-7zm-21.586-2l5.586-5.586 5.586 5.586h-11.172zM180 165.414L174.414 171H180v-5.586zm-8 5.172l5.586-5.586h-11.172l5.586 5.586zM152.933 25.653l1.414 1.414-33.94 33.942-1.416-1.416 33.943-33.94zm1.414 127.28l-1.414 1.414-33.942-33.94 1.416-1.416 33.94 33.943zm-127.28 1.414l-1.414-1.414 33.94-33.942 1.416 1.416-33.943 33.94zm-1.414-127.28l1.414-1.414 33.942 33.94-1.416 1.416-33.94-33.943zM0 85c2.21 0 4 1.79 4 4s-1.79 4-4 4v-8zm180 0c-2.21 0-4 1.79-4 4s1.79 4 4 4v-8zM94 0c0 2.21-1.79 4-4 4s-4-1.79-4-4h8zm0 180c0-2.21-1.79-4-4-4s-4 1.79-4 4h8z' fill='%23eca417' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");
   box-shadow: 20px 20px 0px rgba(0, 0, 0, 0.5);
   box-sizing: border-box;
@@ -351,27 +351,25 @@ const UrlInput = styled.input`
   color: #00aced;
 `;
 
-
 class App extends Component {
   state = {
     currentName: '',
     participants: [],
     currentlySelected: null,
     shuffling: false,
-    popupStart: {top: 0, left: 0},
+    popupStart: { top: 0, left: 0 },
     generatingUrl: false,
     generatingForTwitter: false,
     currentPlaceholder: PLACEHOLDERS[Math.floor(Math.random() * PLACEHOLDERS.length)],
     seed: 0,
     url: '',
+    ...getStoredState(),
   };
   elements = {};
   componentDidMount() {
-    this.setState(() => getStoredState());
-
-    setInterval(
+    this.intervalId = setInterval(
       () => {
-        this.setState(({currentPlaceholder}) => ({
+        this.setState(({ currentPlaceholder }) => ({
           currentPlaceholder: PLACEHOLDERS[
             (PLACEHOLDERS.indexOf(currentPlaceholder) + 1) % PLACEHOLDERS.length
           ],
@@ -383,34 +381,39 @@ class App extends Component {
   componentDidUpdate() {
     writeToUrl(this.state);
   }
-  updateName = event => {
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+  updateName = (event) => {
     const name = event.target.value;
     this.setState(() => ({
       currentName: name,
     }));
   };
-  addParticipant = event => {
+  addParticipant = (event) => {
     event.preventDefault();
     const participantsLength = this.state.participants.length;
 
     const names = this.state.currentName
       .split(/[,;\n]/gi)
-      .map(name => name.trim())
+      .map((name) => name.trim())
       .map((name, i) => ({
         id: participantsLength + i,
         name,
       }));
 
-    this.setState(state => {
-      const participants = state.participants.concat(names);
+    this.setParticipants(this.state.participants.concat(names));
+  };
+  setParticipants = (names) => {
+    this.setState(() => {
       return {
-        participants,
+        participants: names,
         currentName: '',
         url: '',
       };
     });
 
-    if(this.state.shuffling) {
+    if (this.state.shuffling) {
       this.cancelShuffle();
     }
 
@@ -419,9 +422,9 @@ class App extends Component {
     this.setState(() => ({
       winner: null,
       shuffling: false,
-      currentlySelected: null
+      currentlySelected: null,
     }));
-  }
+  };
   loop = () => {
     if (this.state.currentlySelected === this.state.targetIndex) {
       const winnerLotRect = this.elements[this.state.winner.id].getBoundingClientRect();
@@ -438,12 +441,12 @@ class App extends Component {
       return;
     }
 
-    if(!this.state.shuffling) {
+    if (!this.state.shuffling) {
       return;
     }
 
     this.setState(
-      ({participants, currentlySelected}) => ({
+      ({ participants, currentlySelected }) => ({
         currentlySelected: currentlySelected === null ? 0 : currentlySelected + 1,
       }),
       () =>
@@ -456,7 +459,7 @@ class App extends Component {
         ),
     );
   };
-  submitIfEnter = event => {
+  submitIfEnter = (event) => {
     if (!event.shiftKey && event.keyCode === 13) {
       this.addParticipant(event);
     }
@@ -465,23 +468,22 @@ class App extends Component {
     this.elements[participant.id] = el;
   };
   generateShortUrl = (generatingForTwitter = false) => {
-
-    if(this.state.url !== '' && generatingForTwitter) {
+    if (this.state.url !== '' && generatingForTwitter) {
       return Promise.resolve();
     }
 
     this.setState(() => ({
       generatingUrl: true,
       url: '',
-      generatingForTwitter
+      generatingForTwitter,
     }));
 
-    const onShortUrlLoaded = ({url}) =>
+    const onShortUrlLoaded = ({ url }) =>
       new Promise((resolve, reject) =>
         this.setState(
           () => ({
             url,
-            generatingUrl: false
+            generatingUrl: false,
           }),
           resolve,
         ));
@@ -491,18 +493,17 @@ class App extends Component {
         this.setState(
           () => ({
             url: window.location.href,
-            generatingUrl: false
+            generatingUrl: false,
           }),
           resolve,
-        ))
+        ));
 
     return shorten(window.location.href).then(onShortUrlLoaded).catch(onShortUrlFailed);
   };
   shuffle = () => {
+    const { shuffling, participants } = this.state;
 
-    const {shuffling, participants} = this.state;
-
-    if(shuffling) {
+    if (shuffling) {
       return;
     }
 
@@ -515,18 +516,19 @@ class App extends Component {
         shuffling: true,
         url: '',
         seed,
-        targetIndex: Math.floor(MIN_SHUFFLES / participants.length) * participants.length + winnerIndex,
+        targetIndex: Math.floor(MIN_SHUFFLES / participants.length) * participants.length +
+          winnerIndex,
         winner,
       }),
       this.loop,
     );
   };
-  removeParticipant = participant => {
-    if(this.state.shuffling) {
+  removeParticipant = (participant) => {
+    if (this.state.shuffling) {
       this.cancelShuffle();
     }
-    this.setState(({participants}) => ({
-      participants: participants.filter(p => p !== participant),
+    this.setState(({ participants }) => ({
+      participants: participants.filter((p) => p !== participant),
     }));
   };
   closeModal = () => {
@@ -584,7 +586,7 @@ class App extends Component {
                 this.state.currentlySelected % this.state.participants.length === i;
               return (
                 <Lot
-                  innerRef={el => this.setRef(el, participant)}
+                  innerRef={(el) => this.setRef(el, participant)}
                   winner={isWinner}
                   highlighted={highlighted}
                   key={participant.id}
@@ -635,7 +637,7 @@ class App extends Component {
               </WinnerPopupOpening>
               <WinnerPopupOpen className="winner-popup-open-content">
                 <WinnerLabel>
-                  {/*Paras mahdollinen vaihtoehto on:*/}
+                  {/* Paras mahdollinen vaihtoehto on:*/}
                   Ja voittaja on:
                 </WinnerLabel>
                 <WinnerText>
@@ -676,16 +678,16 @@ class App extends Component {
                       </FacebookShareButtonText>
                     </FacebookShareButtonContent>
                   </TwitterShareButtonContainer>
-                  {this.state.url === '' || this.state.generatingForTwitter ? (
-                    <UrlShareButtonContainer onClick={() => this.generateShortUrl()}>
+                  {this.state.url === '' || this.state.generatingForTwitter
+                    ? <UrlShareButtonContainer onClick={() => this.generateShortUrl()}>
                       {this.state.generatingUrl && !this.state.generatingForTwitter
-                        ? <img src={loader} alt="Loading" />
-                        : <UrlIcon />}
+                          ? <img src={loader} alt="Loading" />
+                          : <UrlIcon />}
                       <FacebookShareButtonText>
-                        Jaa linkki
-                      </FacebookShareButtonText>
+                          Jaa linkki
+                        </FacebookShareButtonText>
                     </UrlShareButtonContainer>
-                  ) : <UrlInput readOnly value={this.state.url} />}
+                    : <UrlInput readOnly value={this.state.url} />}
                 </Social>
                 <CloseModal onClick={this.closeModal}>
                   <CloseIcon src={close} alt="Sulje" />
