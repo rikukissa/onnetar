69f9835f42ccf9d6830f07a64b666f3b2c32d975
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-25 12:00:47 +0200

    Updates

---
+++ precache-manifest.f3ac98227742447fc29e9ad795126663.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d117f1fb04da21a4d345389456bef53",
+    "url": "/static/media/chicken.5d117f1f.svg"
+  },
+  {
+    "revision": "12287175c80c5145137a",
+    "url": "/static/css/main.2b6202a7.chunk.css"
+  },
+  {
+    "revision": "94990d9e16bff81ca2e5",
+    "url": "/static/js/1.94990d9e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cbfa1b083bc36969c4ecf5b273878ad8",
+    "url": "/static/media/loader.cbfa1b08.svg"
+  },
+  {
+    "revision": "68e05e04bb9a53c4ccb090bb0931dbee",
+    "url": "/static/media/logo.68e05e04.svg"
+  },
+  {
+    "revision": "12287175c80c5145137a",
+    "url": "/static/js/main.12287175.chunk.js"
+  },
+  {
+    "revision": "3156a7108c65aab82bd5c3720a59d43c",
+    "url": "/static/media/link.3156a710.svg"
+  },
+  {
+    "revision": "9d92e77d2ae66672c865ec3dff735c09",
+    "url": "/static/media/logo-text.9d92e77d.svg"
+  },
+  {
+    "revision": "dfa163a014da1d1d88221d5717bef7b3",
+    "url": "/static/media/step1.dfa163a0.svg"
+  },
+  {
+    "revision": "710c6076b41a6a98a2ae645bf77e9c08",
+    "url": "/static/media/step2.710c6076.svg"
+  },
+  {
+    "revision": "8bcac34c40e2a9ac6903bc3394aa9342",
+    "url": "/static/media/step3.8bcac34c.svg"
+  },
+  {
+    "revision": "fc7bd7d36dbe956851584058309f8f45",
+    "url": "/index.html"
+  }
+];
