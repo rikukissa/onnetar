eb9b4bcc581d5f4ffe1c56a2752093141f6f73c1
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-27 18:01:09 +0200

    add code to test service worker

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ if (module.hot) {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-console.log("version 10");
+console.log("version 11");
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.register({
   onUpdate: async registration => {
-    alert("updated 10");
+    alert("updated 11");
     window.location.reload();
   }
 });
