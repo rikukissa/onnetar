f190d9c3223266d7505b15fa141279534f4323e3
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-27 14:14:38 +0200

    add code to test service worker

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ if (module.hot) {
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
+console.log("version 1");
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
