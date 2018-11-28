6da5fbd762174b7cb19a723597f9329d81db0b84
Riku Rouvila <riku.rouvila@gmail.com>
2018-11-28 10:51:23 +0200

    add standalone check for android

--- src/mobile.ts
+++ src/mobile.ts
@@ -1,3 +1,5 @@
 export const MOBILE_WIDTH = 500;
-export const STANDALONE = (window.navigator as any).standalone;
+export const STANDALONE =
+  (window.navigator as any).standalone ||
+  window.matchMedia("(display-mode: standalone)").matches;
 export const IS_MOBILE = window.innerWidth < MOBILE_WIDTH;
